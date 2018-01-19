dc196b31e9eefc97e66e6b1a48b0f28c35b9a1e0
TylerBynum <tb0992562@RK03-B1225-05.francistuttle.edu>
2018-01-19 08:46:58 -0600

    First file version

---
+++ src/ProgramSeven.java
@@ -0,0 +1,34 @@
+
+//Peyton Annett
+import java.util.Scanner;
+
+public class ProgramSeven { 
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many numbers will be read: ");
+        int number = input.nextInt();
+        int[] array = new int[number];
+        System.out.println("Enter " + array.length + " elements: ");
+
+        filla(array, input);
+        
+        System.out.println("The contents of the array: ");
+        System.out.print(java.util.Arrays.toString(array));
+
+    }
+
+    public static void filla(int[] array, Scanner input) {
+        int total = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+        for (int i = 0; i < array.length; i++) {
+            total += array[i];
+        }
+        double average = (total / array.length);
+        System.out.printf("The average is: %.1f", average);
+        System.out.printf("\n");
+           }
+}
